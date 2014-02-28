d701446d98d5362daf3b7d5fc14197d346e77608
Tom Parker <tom.ed.parker@gmail.com>
2014-02-28 16:30:22 +0000

    updated git ignore to ignore DS_Store, copied original.cpp to working files main.cpp, processes.cpp and processes.hpp, updated Makefile to reflect this change and added test and all rules

--- makefile
+++ makefile
@@ -1,8 +1,15 @@
-CPPFLAGS = -std=c++11 -stdlib=libstdc++ -framework OpenCL -I include
+CPPFLAGS = -std=c++11 -stdlib=libstdc++ -I include
 
 bin/original : src/original/original.cpp
 	clang++ $(CPPFLAGS) $^ -o $@ 
 	
+bin/process : src/main.cpp src/processes.cpp
+	clang++ $(CPPFLAGS)  src/main.cpp src/processes.cpp -o $@ 
+	
 test : bin/original
-	convert lenna.png -depth 2 gray:- | ./bin/original 512 512 2 > output.raw
+	time (convert lenna.png -depth 2 gray:- | ./bin/original 512 512 2 > output_original.raw)
+	time (convert lenna.png -depth 2 gray:- | ./bin/process 512 512 2 > output_process.raw)
+	diff output_original.raw output_process.raw
+	
+all : bin/process bin/original
 	
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include "processes.hpp"
+#include <unistd.h>
+#include <algorithm>
+#include <cassert>
+#include <stdexcept>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	try{
+		if(argc<3){
+			fprintf(stderr, "Usage: process width height [bits] [levels]\n");
+			fprintf(stderr, "   bits=8 by default\n");
+			fprintf(stderr, "   levels=1 by default\n");
+			exit(1);
+		}
+		
+		unsigned w=atoi(argv[1]);
+		unsigned h=atoi(argv[2]);
+		
+		unsigned bits=8;
+		if(argc>3){
+			bits=atoi(argv[3]);
+		}
+		
+		if(bits>32)
+			throw std::invalid_argument("Bits must be <= 32.");
+		
+		unsigned tmp=bits;
+		while(tmp!=1){
+			tmp>>=1;
+			if(tmp==0)
+				throw std::invalid_argument("Bits must be a binary power.");
+		}
+		
+		if( ((w*bits)%64) != 0){
+			throw std::invalid_argument(" width*bits must be divisible by 64.");
+		}
+		
+		int levels=1;
+		if(argc>4){
+			levels=atoi(argv[4]);
+		}
+		
+		fprintf(stderr, "Processing %d x %d image with %d bits per pixel.\n", w, h, bits);
+		
+		uint64_t cbRaw=uint64_t(w)*h*bits/8;
+		std::vector<uint64_t> raw(cbRaw/8);
+		
+		std::vector<uint32_t> pixels(w*h);
+		
+		while(1){
+			if(!read_blob(STDIN_FILENO, cbRaw, &raw[0]))
+				break;	// No more images
+			unpack_blob(w, h, bits, &raw[0], &pixels[0]);		
+			
+			process(levels, w, h, bits, pixels);
+			//invert(levels, w, h, bits, pixels);
+			
+			pack_blob(w, h, bits, &pixels[0], &raw[0]);
+			write_blob(STDOUT_FILENO, cbRaw, &raw[0]);
+		}
+		
+		return 0;
+	}catch(std::exception &e){
+		std::cerr<<"Caught exception : "<<e.what()<<"\n";
+		return 1;
+	}
+}
